49f2bbde32178a1137c9c2f5d0032886c9c7f2fe
whale <3121279905@qq.com>
2024-01-14 12:21:07 +0800

    静态数据tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import TabBar from "@/components/TabBar.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>APP</h2>
+  <router-view/>
+  <tab-bar></tab-bar>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'normalize.css'  //样式重置
+import './assets/css/index.css'  //引入样式设置
+
+import router from './routers/index.js' //引入路由
+
+import pinia from './stores/index.js' //使用pinia状态管理
+
+createApp(App).use(router).use(pinia).mount('#app')
