0f5cabaaec4cf898a6bf01f47d38add7388920ea
whale <3121279905@qq.com>
2024-01-15 21:43:57 +0800

    数据处理与索引栏设计

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,6 +1,11 @@
 <script setup>
-  import {ref} from "vue";
-  import {useRouter} from "vue-router";
+  import { computed, ref } from "vue";
+  import { useRouter } from "vue-router";
+  import useCityStore from "@/stores/modules/city.js";
+  import { storeToRefs } from "pinia";
+  import CityGroup from "@/views/city/cpns/city-group.vue";
+
+
   const router=useRouter()
 
   //搜索栏数据
@@ -35,19 +40,22 @@
 
 
   //3.使用pinia状态管理
-  import useCityStore from "@/stores/modules/city.js";
-  import {storeToRefs} from "pinia";
   const cityStore = useCityStore()
   //触发网络请求
   cityStore.fetchAllCities()
-  //进行解析,响应式
+  //进行解析,响应式数据
   const { allCities }= storeToRefs(cityStore)
+
+  //获取选中标签的数据【国内 /  海外】
+  const currentGroup =computed(()=>allCities.value[tabActive.value])
 </script>
 
 <template>
   <div class="city top-tabbar-hide">
-    <!-- 1.搜索栏-->
-    <div class="search">
+    <!--顶部-->
+    <div class="top">
+      <!-- 1.搜索栏-->
+      <div class="search">
         <van-search
             v-model="searchValue"
             show-action
@@ -55,26 +63,64 @@
             placeholder="城市/区域/位置"
             @cancel="cancelClick"
         />
+      </div>
+      <!-- 2.切换tag-->
+      <van-tabs v-model:active="tabActive" color="#ff9854">
+        <!--
+            1.网络请求数据，动态渲染 从allCity中
+            一开始allCity的默认值是undefined,所以会报错
+            ?. 语法：有值是会获取，没有值时不会获取
+        -->
+        <!--  <van-tab :title="allCity?.cityGroup?.title"></van-tab>-->
+        <!--  <van-tab :title="allCity?.cityGroupOverSea?.title"></van-tab>-->
+
+        <!-- 2.v-for循环allCity对象   -->
+        <template v-for="(value,key,index) in allCities" :key="key ">
+                                    <!-- name属性与tabActive
+                                         在标签指定 name 属性的情况下，v-model:tabActive 的值为当前标签的 name
+                                         这样就可以获取选中标签的数据【国内 /  海外】
+                                    -->
+          <van-tab :title="value.title" :name="key"></van-tab>
+        </template>
+      </van-tabs>
     </div>
-    <!-- 2.切换tag-->
-    <van-tabs v-model:active="tabActive" color="#ff9854">
-    <!--
-        1.网络请求数据，动态渲染 从allCity中
-        一开始allCity的默认值是undefined,所以会报错
-        ?. 语法：有值是会获取，没有值时不会获取
-    -->
-    <!--  <van-tab :title="allCity?.cityGroup?.title"></van-tab>-->
-    <!--  <van-tab :title="allCity?.cityGroupOverSea?.title"></van-tab>-->
 
-    <!-- 2.v-for循环allCity对象   -->
-      <template v-for="(value,key,index) in allCities" :key="key ">
-        <van-tab :title="value.title"></van-tab>
+    <div class="context">
+        <!--  <city-group :group-data="currentGroup"/>-->
+        <!--  这样进行切换时，currentGroup会重新赋值，组件重新渲染时间较长-->
+
+        <!--用v-show来展示可以优化这个问题-->
+      <template v-for="(value,key,index) in allCities">
+        <city-group :group-data="value" v-show="tabActive === key"/>
       </template>
-    </van-tabs>
-
+    </div>
   </div>
 </template>
 
 <style scoped>
+    /*
+      顶部固定，内容滚动
+      1.设置top固定,缺点：滚动条位置不好
+    .top{
+      position: fixed;
+      top:0;
+      left: 0;
+      right: 0;
+    }
+    .context{
+      margin-top: 98px;
+    }
+    */
+    .top{
+      position:relative;
+      z-index:9;
+    }
+    /*
+      2.局部滚动，页面正常布局，让内容高度固定，
+    */
+    .context{
+      height: calc(100vh - 98px);
 
+      overflow-y: auto;
+    }
 </style>
---
+++ src/views/city/cpns/city-group.vue
@@ -0,0 +1,76 @@
+<script setup>
+  import {computed} from "vue";
+
+  const props = defineProps({
+    groupData:{
+      type:Object,
+      default:()=>({})
+    }
+  })
+
+  // index-list 	索引字符列表
+  const indexList =computed(()=>{
+    const list = props.groupData.cities.map( item => item.group )
+    //在前面添加 # 表示热门索引
+    list.unshift("#")
+    return list
+  })
+</script>
+
+<template>
+  <div class="city-group">
+    <!-- index-list 	索引字符列表-->
+    <van-index-bar :index-list="indexList">
+      <van-index-anchor index="热门" />
+      <div class="list">
+        <template v-for="(item,index) in groupData.hotCities" :key="index">
+          <div class="hot-city">
+            {{ item.cityName }}
+          </div>
+        </template>
+      </div>
+
+      <template v-for="(item,index) in groupData.cities" :key="index">
+        <van-index-anchor :index="item.group" />
+        <template v-for="(cityItem,cityIndex) in item.cities">
+          <div class="city">
+            <van-cell :title="cityItem.cityName" />
+          </div>
+        </template>
+      </template>
+    </van-index-bar>
+
+<!--    <template v-for="(item,index) in groupData.cities" :key="index">-->
+<!--      <div class="group-item">-->
+<!--        <h2 class="title">{{ item.group }}</h2>-->
+<!--        <div class="city-list">-->
+<!--          <template v-for="(cityItem,cityIndex) in item.cities">-->
+<!--            <div class="city">-->
+<!--              {{ cityItem.cityName }}-->
+<!--            </div>-->
+<!--          </template>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </template>-->
+  </div>
+</template>
+
+<style scoped>
+.list{
+  display: flex;
+  flex-wrap:wrap;
+  justify-content: space-around;
+  margin: 10px 25px 10px 10px;
+}
+.hot-city{
+  width:70px;
+  height: 28px;
+  font-size: 12px;
+  color:#000;
+  background-color: #fff4ec;
+  border-radius: 14px;
+  text-align: center;
+  line-height: 28px;
+  margin:6px 0;
+}
+</style>
